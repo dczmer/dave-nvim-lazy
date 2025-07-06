8302f8caa789c2626437a78c3e0e5cefc7f6e6e6
Dave Czmer <dczmer@gmail.com>
2025-07-06 18:44:26 -0400

    Enable word-wrap when opening markdown files.

--- config/lua/dave-vim/commands.lua
+++ config/lua/dave-vim/commands.lua
@@ -10,3 +10,7 @@ vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
     },
     command = "setlocal tabstop=2 | setlocal softtabstop=2 | setlocal shiftwidth=2",
 })
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+    pattern = { "*.md" },
+    command = "setlocal wrap",
+})
