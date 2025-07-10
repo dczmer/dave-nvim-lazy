aad230e19df8ffc2ffd8cc4a6fe4145c1967ea35
Dave Czmer <dczmer@cisco.com>
2025-07-10 11:14:16 -0400

    Fix camelcasemotion keys.

--- config/lua/dave-vim/plugins/camelcasemotion.lua
+++ config/lua/dave-vim/plugins/camelcasemotion.lua
@@ -1,4 +1,17 @@
-vim.keymap.set("n", "w", "<Plug>CamelCaseMotion_w")
-vim.keymap.set("n", "b", "<Plug>CamelCaseMotion_b")
-vim.keymap.set("n", "e", "<Plug>CamelCaseMotion_e")
-vim.keymap.set("n", "ge", "<Plug>CamelCaseMotion_ge")
+vim.cmd([[
+    map <silent> w <Plug>CamelCaseMotion_w
+    map <silent> b <Plug>CamelCaseMotion_b
+    map <silent> e <Plug>CamelCaseMotion_e
+    map <silent> ge <Plug>CamelCaseMotion_ge
+    sunmap w
+    sunmap b
+    sunmap e
+    sunmap ge
+
+    omap <silent> iw <Plug>CamelCaseMotion_iw
+    xmap <silent> iw <Plug>CamelCaseMotion_iw
+    omap <silent> ib <Plug>CamelCaseMotion_ib
+    xmap <silent> ib <Plug>CamelCaseMotion_ib
+    omap <silent> ie <Plug>CamelCaseMotion_ie
+    xmap <silent> ie <Plug>CamelCaseMotion_ie
+]])
