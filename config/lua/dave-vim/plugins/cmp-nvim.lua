7e5d9323a6bf724e1d2941b165db5f294f25db80
Dave Czmer <dczmer@gmail.com>
2026-02-06 21:07:42 -0500

    Fix annoying bug with cmp completion replacing the rest of the line.

--- config/lua/dave-vim/plugins/cmp-nvim.lua
+++ config/lua/dave-vim/plugins/cmp-nvim.lua
@@ -34,13 +34,13 @@ local mappings = function(cmp, select_opts)
         ["<CR>"] = cmp.mapping({
             i = function(fallback)
                 if cmp.visible() and cmp.get_active_entry() then
-                    cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+                    cmp.confirm({ select = true })
                 else
                     fallback()
                 end
             end,
             s = cmp.mapping.confirm({ select = true }),
-            c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false }),
+            c = cmp.mapping.confirm({ select = true }),
         }),
         ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
